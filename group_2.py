653e59b432d8d32ace2174bc19e66c7b353fd9c4
ibrahimfakhry <Ibrahim fakhrey@github.com>
2023-04-17 01:33:03 +0200

    i have given group 2 session 6

--- group_2.py
+++ group_2.py
@@ -3,77 +3,42 @@ from flask import Flask, render_template, request, url_for
 from werkzeug.utils import redirect
 
 app = Flask(__name__)
-End_point="https://api.openweathermap.org/data/2.5/weather"
-lat="30.350176"
-lon="31.219482"
-API_KEY="868c5469fc5add3122d3e7e8313c8f3e"
-users={
-    "ahmed":"1234",
-    "ma7moud":"0000",
-    "wa7eed":"1122"
+
+
+data={
+    "img":"https://images.unsplash.com/photo-1661956601031-4cf09efadfce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80",
+    "i": "https://images.unsplash.com/photo-1661956601031-4cf09efadfce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80",
+
 }
 
+rating={
+    "img":1,
+    # "i":2
+}
 
 
 
 @app.route("/")
 def start():
+    images=data
+    return render_template("lessson5.html",img=images)
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+        name = request.form.get("name")
+        link = request.form.get("link")
+        data[name] = link
+        return redirect("/")
+    return render_template("add.html")
 
-    return render_template("start.html")
+@app.route("/show")
+def show():
+    key = request.args.get('key')
+    for k in rating:
+        if k==key:
+            return f"{rating[key]}"
 
-@app.route("/login",methods=["GET","POST"])
-def login():
-    if request.method=="POST":
-        name=request.form.get("name")
-        password=request.form.get("password")
-        for thing in users:
-            if thing ==name:
-                if password==users[thing]:
-                    params = {
-                        "lat": lat,
-                        "lon": lon,
-                        "appid": API_KEY
-                    }
-
-                    response = requests.get(url=End_point, params=params)
-
-                    data = response.json()
-                    print(data)
-
-                    temper = data["main"]["temp"]
-                    celtemp = int(temper) - 273
-                    desc = data["weather"][0]["main"]
-                    hum = data["main"]["humidity"]
-                    city_name = data["name"]
-                    return render_template("weather.html", temp=celtemp, city=city_name)
-
-
-
-                else:
-                    m="your password is incorrect"
-                    return render_template("4.html",m=m)
-        m="you are not with us "
-        return render_template("4.html",m=m)
-
-    return render_template("test.html")
-@app.route("/register",methods=["GET","POST"])
-def register():
-    if request.method=="POST":
-        name=request.form.get("name")
-        password=request.form.get("password")
-        users[name]=password
-        return redirect(url_for('login'))
-
-    return render_template("register.html")
-
-
-
-
-
-
-
-
-
+    return f" i didnot find a rating for {key}"
 
 
 
