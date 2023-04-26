bcc45e0049aca2dfea6a692ad3becdd6f8b55cf8
ibrahimfakhry <Ibrahim fakhrey@github.com>
2023-04-26 13:03:14 +0200

    i have given group 1 session 5

--- main.py
+++ main.py
@@ -1,61 +1,45 @@
 import requests
 from flask import Flask,render_template,request
+from werkzeug.utils import redirect
+
 app = Flask(__name__)
-End_point="https://api.openweathermap.org/data/2.5/weather"
-lat="30.350176"
-lon="31.219482"
-API_KEY="868c5469fc5add3122d3e7e8313c8f3e"
-users={
-    "hana":"1234",
-    "bosy":"0000",
-    "merhan":"1122"
+
+images={
+    "1":"https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=874&q=80",
+    "2":"https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=874&q=80",
+    "3":"https://images.unsplash.com/photo-1514525253161-7a46d19cd819?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
 }
-data={
-
-
+rating={
+    "1":20,
+    "2":0,
+    "3":0
 }
 
-@app.route("/",methods=["GET","POST"])
-def wall():
-    if request.method=="POST":
-        user_name=request.form.get("name")
-        user_comment=request.form.get("comment")
-        #write your code below
-        data[user_name]=user_comment
-        print(data)
 
-        return render_template("wall.html",items=data)
-    return render_template("lesson3.html")
-@app.route("/getting weather",methods=["GET","POST"])
+@app.route("/")
 def start():
+
+
+    return render_template("session5.html",img=images)
+
+@app.route("/show")
+def show ():
+    key=request.args.get('key')
+    print(key)
+    for k in rating :
+        if k==key:
+            return f"{rating[key]}"
+    return "i didnot find rating for this "
+@app.route("/add",methods=["GET","POST"])
+def add():
     if request.method=="POST":
         name=request.form.get("name")
-        password=request.form.get("password")
-        for i in users:
-            if i==name:
-                if users[i]==password:
+        link=request.form.get("link")
+        images[name]=link
+        return redirect("/")
 
-                    params = {
-                        "lat": lat,
-                        "lon": lon,
-                        "appid": API_KEY
-                    }
 
-                    response = requests.get(url=End_point, params=params)
-
-                    data = response.json()
-                    print(data)
-
-                    temper = data["main"]["temp"]
-                    celtemp = int(temper) - 273
-                    desc = data["weather"][0]["main"]
-                    hum = data["main"]["humidity"]
-                    city_name = data["name"]
-                    return render_template("weather.html", temp=celtemp, city=city_name)
-        else:
-            return "ERROR 404"
-    return render_template("index.html")
-
+    return render_template("add.html")
 
 
 
